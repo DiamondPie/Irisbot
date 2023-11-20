6fa197b3088e3f7eb6f3d34ec60ebee6a7f7fc53
DiamondPie <137748098+DiamondPie@users.noreply.github.com>
2023-11-20 19:47:43 +1300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,5 +1,5 @@
 version="1.1.2"
-discord_token="MTE2MjIxNTExMTM1MjY0Nzc3MQ.G0gs4A.D5Z6vndpAYwLHL0ABZNZUawroHxadAARXOJxic"
+discord_token="在这粘贴你的discord bot token"
 replicate_token="r8_0Qar5xa2sSHNMkKlWXhv3JYXEGAqafX3jGtaP"
 ninjas_token="L3cZ4YmgAAuqwR8Ao0yryw==EVFKsYpsHlENzHk4"
 yunxi_token="683dcd4201de79e4a2002b6ef78a40d9"
@@ -20,18 +20,3 @@ ffmpeg_options={
     'before_options': '-reconnect 1 -reconnect_streamed 1 -reconnect_delay_max 5', 
     'options': '-vn'
 }
-# import yaml
-
-# with open("./config.yml", 'r') as f:
-#     data = yaml.safe_load(f)
-
-# print(data)
-# print(data["token"]["discord"])
-# print(type(data['token']['discord']))
-
-# original_dict = {"a": 1, "b": 2}
-# suffix = "_test"
-
-# new_dict = {key + suffix: value for key, value in original_dict.items()}
-
-# print(new_dict)
